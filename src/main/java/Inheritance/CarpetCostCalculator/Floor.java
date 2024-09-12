f85b69465c9ed21e09a9348b89508c19f92dfa46
jwiszowaty <jakubwiszowaty@hotmail.com>
2024-09-12 16:18:31 +0100

    add completed exercises in Inheritance section of Udemy course

---
+++ src/main/java/Inheritance/CarpetCostCalculator/Floor.java
@@ -0,0 +1,14 @@
+package Inheritance.CarpetCostCalculator;
+
+public class Floor {
+    private double width, length;
+
+    public Floor(double width, double length) {
+        this.width = width < 0 ? 0 : width;
+        this.length = length < 0 ? 0 : length;
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+}
---
+++ src/main/java/Inheritance/ComplexNumber.java
@@ -0,0 +1,33 @@
+package Inheritance;
+
+public class ComplexNumber {
+    private double real, imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return this.real;
+    }
+    public double getImaginary() {
+        return this.imaginary;
+    }
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+    public void add(ComplexNumber complexNumber) {
+        this.real += complexNumber.real;
+        this.imaginary += complexNumber.imaginary;
+    }
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+    public void subtract(ComplexNumber complexNumber) {
+        this.real -= complexNumber.real;
+        this.imaginary -= complexNumber.imaginary;
+    }
+}
