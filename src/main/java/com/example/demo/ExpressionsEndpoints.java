9898a69ee12f289f2b7c9ada782e392073fe6fca
jwiszowaty <jakubwiszowaty@hotmail.com>
2024-10-04 12:38:18 +0100

    area calculator /area-calc endpoint added and deployed

--- src/main/java/com/example/demo/ExpressionsEndpoints.java
+++ src/main/java/com/example/demo/ExpressionsEndpoints.java
@@ -10,14 +10,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api")
 public class ExpressionsEndpoints {
 
-    @GetMapping("/area-calc") // checked // added
+    @GetMapping("/area-calc") // checked
     public ResponseEntity<String> calculateArea(
-            @RequestParam(value = "number1") int number1,
-            @RequestParam(value = "number2") int number2) {
+            @RequestParam(value = "number1") double number1,
+            @RequestParam(value = "number2", required = false) double number2) {
         try {
             return ResponseEntity
                     .status(200)
-                    .body(AreaCalculator.area(number1, number2));
+                    .body(Double.toString(number2 > 0 ? AreaCalculator.area(number1,number2) :AreaCalculator.area(number1) ));
         } catch (Exception e) {
             return ResponseEntity
                     .status(400)
@@ -25,18 +25,16 @@ public class ExpressionsEndpoints {
         }
     }
 
-    @GetMapping("/first-last-digit-sum/{number}") // checked // added
-    public ResponseEntity<String> sumFirstAndLastDigit(@PathVariable int number) {
-        try {
-            return ResponseEntity
-                    .status(200)
-                    .body(FirstLastDigitSum.sumFirstAndLastDigit(number));
-        } catch (Exception e) {
-            return ResponseEntity
-                    .status(400)
-                    .body(e.getMessage());
-        }
-    }
-}
-
+//    @GetMapping("/") // checked // added
+//    public ResponseEntity<String> sumFirstAndLastDigit(@PathVariable int number) {
+//        try {
+//            return ResponseEntity
+//                    .status(200)
+//                    .body(FirstLastDigitSum.sumFirstAndLastDigit(number));
+//        } catch (Exception e) {
+//            return ResponseEntity
+//                    .status(400)
+//                    .body(e.getMessage());
+//        }
+//    }
 }
